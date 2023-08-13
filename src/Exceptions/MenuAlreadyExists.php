ee0cf0c55ed3d964844fb06086cc80b5f5d70ff3
balajidharma <balajidharma@hotmail.com>
2023-08-12 21:49:04 -0400

    Updated exception message

--- src/Exceptions/MenuAlreadyExists.php
+++ src/Exceptions/MenuAlreadyExists.php
@@ -8,6 +8,6 @@ class MenuAlreadyExists extends InvalidArgumentException
 {
     public static function create(string $machineName)
     {
-        return new static("A `{$machineName}` permission already exists.");
+        return new static("A `{$machineName}` already exists.");
     }
 }
